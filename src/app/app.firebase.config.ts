713806051eb6addbc37611163ca86246d5a8b972
Andry <andriramananarivo@gmail.com>
2018-07-05 16:11:33 +0500

    firebase and facebook package

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+	apiKey: "AIzaSyB8BG36MalAlcQ7ye6WOtjZvLadaofOO0c",
+	authDomain: "crud-ccdaa.firebaseapp.com",
+	databaseURL: "https://crud-ccdaa.firebaseio.com",
+	projectId: "crud-ccdaa",
+	storageBucket: "crud-ccdaa.appspot.com",
+	messagingSenderId: "566261345441"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  BrowserModule,
+  AngularFireModule.initializeApp(FIREBASE_CONFIG),
+  AngularFireDatabaseModule,
+  IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  StatusBar,
+  SplashScreen,
+  {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
