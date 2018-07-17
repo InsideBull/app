951b6af2b57ae6ccdc46e0d46ff94c529f3221fc
Andry <andriramananarivo@gmail.com>
2018-07-17 15:39:07 +0500

    car-type model

---
+++ src/models/car-type.model.ts
@@ -0,0 +1,14 @@
+export class CarType{
+	type : string;
+	nbplace ?: number;
+	placelist ?: string;
+	notavailable ?: string;
+	nbRows ?: number;
+	nbCols ?: number;
+
+	constructor(object?: {}) {
+		for (var key in object) {
+			this[key] = object[key];
+		}
+	}
+}
