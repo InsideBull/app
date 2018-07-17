0ebeb5dc5a8eed5b67cc2af149568150b7509431
Andry <andriramananarivo@gmail.com>
2018-07-17 15:25:07 +0500

    voyage create

--- src/pages/voyage-create/voyage-create.ts
+++ src/pages/voyage-create/voyage-create.ts
@@ -15,71 +15,74 @@ import {Cooperative } from '../../models/cooperative.model'
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-voyage-create',
-  templateUrl: 'voyage-create.html',
-})
-export class VoyageCreatePage {
+ @IonicPage()
+ @Component({
+   selector: 'page-voyage-create',
+   templateUrl: 'voyage-create.html',
+ })
+ export class VoyageCreatePage {
 
-	form: FormGroup;
-	stations: any;
-	voyage: Voyage;
-        param : any;
-        cooperative: Cooperative;
+   form: FormGroup;
+   stations: any;
+   voyage: Voyage;
+   param : any;
+   cooperative: Cooperative = new Cooperative();
 
-  constructor(public navCtrl: NavController, 
-  	public navParams: NavParams,
-  	public voyageProvider: VoyageProvider,
-  	public formBuilder: FormBuilder,
-  	public stationProvider: StationProvider,
-        public cooperativeProvider: CooperativeProvider) {
+   constructor(public navCtrl: NavController, 
+     public navParams: NavParams,
+     public voyageProvider: VoyageProvider,
+     public formBuilder: FormBuilder,
+     public stationProvider: StationProvider,
+     public cooperativeProvider: CooperativeProvider) {
 
-  	this.form = this.formBuilder.group({
- 			arrivalstation: ['',Validators.required],
- 			cooperative: ['', ],
- 			date: ['',Validators.required],
- 			price: [,Validators.required],
- 			startstation: ['',Validators.required]
- 		});
-                
-      
-  }
-
-  ionViewDidLoad() {
-    this.stations = [];    
-    this.param = this.navParams.get('key');
-
-    
-    this.stationProvider.fetcAll().subscribe(
-    	(data) => {
-    		for(let key in data){
-  				data[key].key = key;
-  				this.stations.push(data[key]);
-  			}
-    	});
-    
-          
-      this.cooperativeProvider.fetch(this.param).then(
-      (data: Cooperative) => {
-          this.cooperative = data;
-        });
-        
-  }
-
-  onSubmit(){
-    if(this.form.valid){
-
-      this.form.value.cooperative = this.param;
-  		
-      this.voyage = new Voyage(this.form.value);
-
-      console.log(this.voyage);
-
-  		let key = this.voyageProvider.save(this.voyage);
-  		this.navCtrl.push(VoyageDetailPage, {key: key});
-  	}
-  }
+     this.form = this.formBuilder.group({
+       arrivalstation: ['',Validators.required],
+       cooperative: ['', ],
+       date: ['',Validators.required],
+       price: [,Validators.required],
+       startstation: ['',Validators.required]
+     });
 
 
-}
+   }
+
+   ionViewDidLoad() {
+     this.stations = [];    
+
+     this.param = this.navParams.get('key');
+
+     this.cooperativeProvider.fetch(this.param).then(
+       (data: Cooperative) => {
+         this.cooperative = data;
+
+         console.log(this.cooperative);
+       });
+
+     this.stationProvider.fetcAll().subscribe(
+       (data) => {
+         for(let key in data){
+           data[key].key = key;
+           this.stations.push(data[key]);
+         }
+       });
+
+
+
+   }
+
+   onSubmit(){
+     if(this.form.valid){
+
+       this.form.value.cooperative = this.param;
+
+       this.voyage = new Voyage(this.form.value);
+
+       console.log(this.voyage);
+
+       let key = this.voyageProvider.save(this.voyage);
+       this.navCtrl.push(VoyageDetailPage, {key: key});
+     }
+   }
+
+
+ }
