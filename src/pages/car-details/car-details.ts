319ec865ac9dd3e09c0a5ceeb722f0cb2a66683e
Andry <andriramananarivo@gmail.com>
2018-07-18 16:49:50 +0500

    car details design

--- src/pages/car-details/car-details.ts
+++ src/pages/car-details/car-details.ts
@@ -13,73 +13,132 @@ import { CarListPage } from '../car-list/car-list';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-car-details',
-  templateUrl: 'car-details.html',
-})
-export class CarDetailsPage {
+ @IonicPage()
+ @Component({
+   selector: 'page-car-details',
+   templateUrl: 'car-details.html',
+ })
+ export class CarDetailsPage {
 
-  key: any;
-  coop: any;
-  car: Car = new Car();
-  cartype: any;
+   key: any;
+   coop: any;
+   car: Car = new Car();
+   cartype: any;
+   rows: string[] = [];
+   cols: number[]  = [];
+   alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   reserved: string[] = [];
+   notavailable: string[] = [];
+   nbRows: number;
+   nbCols: number;
 
-  constructor(private carProvider: CarProvider, 
-    public cartypeProvider: CarTypeProvider,
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public alertCtrl: AlertController) {
-  }
+   constructor(private carProvider: CarProvider, 
+     public cartypeProvider: CarTypeProvider,
+     public navCtrl: NavController, 
+     public navParams: NavParams,
+     public alertCtrl: AlertController) {
+   }
 
-  ionViewDidLoad() {
-    this.key = this.navParams.get('key');
-    this.coop = this.navParams.get('coop');
+   ionViewDidLoad() {
+     this.key = this.navParams.get('key');
+     this.coop = this.navParams.get('coop');
 
-    let customPath = `cooperative/${this.coop}/car`;
-    this.carProvider.customPath(customPath);
+     let customPath = `cooperative/${this.coop}/car`;
+     this.carProvider.customPath(customPath);
 
-    this.cartype = {};
-    
-    this.carProvider.fetch(this.key).then(
-      (data: Car)=>{
-          this.cartypeProvider.fetch(data.type).then((cartype)=>{
-            this.car = data;
-            if (!this.car.image) {
-                  this.car.image = "assets/icon/bus.png";
-                }    
-            this.cartype = cartype;     
-          });
-      });
-  }
+     this.cartype = {};
 
-  editer(){
-    this.navCtrl.push(CarEditPage, {key: this.key, coop: this.coop});
-  }
+     this.carProvider.fetch(this.key).then(
+       (data: Car)=>{
+         this.cartypeProvider.fetch(data.type).then((cartype)=>{
+           this.car = data;
 
-  delete(){
-      
-    let alert = this.alertCtrl.create({
-      title: 'Suppression',
-      message: 'Voulez vous supprimer la voiture N° ' + this.car.matricule + ' ?',
-      buttons: [
-        {
-          text: 'annuler',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'supprimer',
-          handler: () => {
-            this.carProvider.deleteCar(this.key);
-            this.navCtrl.push(CarListPage, {key: this.coop});
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
 
-}
+
+
+           if (!this.car.image) {
+             this.car.image = "assets/icon/bus.png";
+           }    
+
+
+           this.cartype = cartype;
+
+
+           this.showingSeats()
+         });
+       });
+   }
+
+
+   showingSeats(){
+
+     let _occuped = [];
+     let car = this.car;
+
+     if(car.occuped){
+       _occuped = JSON.parse(car.occuped);
+       for( let key in _occuped ){
+         this.reserved.push(_occuped[key]);
+       }
+       
+     }
+
+
+     let type = this.cartype;    
+     this.nbCols = type.nbCols;
+     this.nbRows = type.nbRows;
+     this.notavailable = JSON.parse(type['notavailable']);
+     this.rows = [];
+     this.cols = [];
+     let refRows = this.alpha.split('');      
+     for( let i = 0; i < this.nbRows; i++){
+       this.rows.push(refRows[i]);
+     }
+     for( let j = 0; j < this.nbCols; j++){
+       this.cols.push(j);
+     } 
+     
+   }
+
+   getStatus = function(seatPos: string) {
+
+     if( this.notavailable.indexOf(seatPos) !== -1 ){
+       return 'notavailable';
+     }
+
+     if(this.reserved.indexOf(seatPos) !== -1) {
+       return 'reserved';
+     } 
+
+   }
+
+   editer(){
+     this.navCtrl.push(CarEditPage, {key: this.key, coop: this.coop});
+   }
+
+   delete(){
+
+     let alert = this.alertCtrl.create({
+       title: 'Suppression',
+       message: 'Voulez vous supprimer la voiture N° ' + this.car.matricule + ' ?',
+       buttons: [
+       {
+         text: 'annuler',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       },
+       {
+         text: 'supprimer',
+         handler: () => {
+           this.carProvider.deleteCar(this.key);
+           this.navCtrl.push(CarListPage, {key: this.coop});
+         }
+       }
+       ]
+     });
+     alert.present();
+   }
+
+ }
--- src/pages/car-edit/car-edit.ts
+++ src/pages/car-edit/car-edit.ts
@@ -15,81 +15,81 @@ import { CameraProvider } from '../../providers/camera/camera';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-car-edit',
-  templateUrl: 'car-edit.html',
-})
-export class CarEditPage {
-  form: FormGroup;
-  key: string;
-  coop: string;
-  cartypes: any;
-  car: Car = new Car();
-  statusList = ['available', 'breakdown', 'travelling'];
- 	nbplace: number = 0;
- 	image :any;
+ @IonicPage()
+ @Component({
+   selector: 'page-car-edit',
+   templateUrl: 'car-edit.html',
+ })
+ export class CarEditPage {
+   form: FormGroup;
+   key: string;
+   coop: string;
+   cartypes: any;
+   car: Car = new Car();
+   statusList = ['available', 'breakdown', 'travelling'];
+   nbplace: number = 0;
+   image :any;
    url:any;
    type: any;
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams,
-    public formBuilder: FormBuilder,
-    public carProvider: CarProvider,
-    public carTypeProvider: CarTypeProvider,
-    public cameraProvider: CameraProvider,) {
-      this.form = formBuilder.group({
-        matricule: ['',Validators.required],			
-        cartype: ['',Validators.required],	
-        nbplace: [0,Validators.required],	
-        status: ['',Validators.required]
-      })
-  }
+   constructor(public navCtrl: NavController, 
+     public navParams: NavParams,
+     public formBuilder: FormBuilder,
+     public carProvider: CarProvider,
+     public carTypeProvider: CarTypeProvider,
+     public cameraProvider: CameraProvider,) {
+     this.form = formBuilder.group({
+       matricule: ['',Validators.required],			
+       cartype: ['',Validators.required],	
+       nbplace: [0,Validators.required],	
+       status: ['',Validators.required]
+     })
+   }
 
-  ionViewDidLoad() {
-    this.key = this.navParams.get('key');
-    this.coop = this.navParams.get('coop');
+   ionViewDidLoad() {
+     this.key = this.navParams.get('key');
+     this.coop = this.navParams.get('coop');
 
-    let customPath = `cooperative/${this.coop}/car`;
-    this.carProvider.customPath(customPath);
-    this.carProvider.fetch(this.key).then(
-      (data: Car)=>{
-        this.car = data;
-        if (!this.car.image) {
-          this.car.image = "assets/icon/bus.png";
-          this.url = this.car.image;
-        }
-      }
-    );
+     let customPath = `cooperative/${this.coop}/car`;
+     this.carProvider.customPath(customPath);
+     this.carProvider.fetch(this.key).then(
+       (data: Car)=>{
+         this.car = data;
+         if (!this.car.image) {
+           this.car.image = "assets/icon/bus.png";
+         }
+         this.url = this.car.image;
+       }
+       );
 
-    this.cartypes = [];
-    this.carTypeProvider.fetcAll().subscribe((cartypes)=>{
-      for(let key in cartypes){
-        cartypes[key].key = key;
-        this.cartypes.push(cartypes[key]);
-        if(this.car.type == key){
-          let notavailable = JSON.parse(cartypes[key]['notavailable']);
-          let nbplace = cartypes[key]['nbplace'] - Object.keys(notavailable).length;
-          this.nbplace = nbplace;
-        }
-      }
-    });    
-  }
+     this.cartypes = [];
+     this.carTypeProvider.fetcAll().subscribe((cartypes)=>{
+       for(let key in cartypes){
+         cartypes[key].key = key;
+         this.cartypes.push(cartypes[key]);
+         if(this.car.type == key){
+           let notavailable = JSON.parse(cartypes[key]['notavailable']);
+           let nbplace = cartypes[key]['nbplace'] - Object.keys(notavailable).length;
+           this.nbplace = nbplace;
+         }
+       }
+     });    
+   }
 
-  getNbplace(cartypeKey: string){
-    this.carTypeProvider.fetch(cartypeKey).then((cartype)=>{
-      let notavailable = JSON.parse(cartype['notavailable']);
-      let nbplace = cartype['nbplace'] - Object.keys(notavailable).length;
-      this.nbplace = nbplace;
-    })
-  }
+   getNbplace(cartypeKey: string){
+     this.carTypeProvider.fetch(cartypeKey).then((cartype)=>{
+       let notavailable = JSON.parse(cartype['notavailable']);
+       let nbplace = cartype['nbplace'] - Object.keys(notavailable).length;
+       this.nbplace = nbplace;
+     })
+   }
 
-  onSubmit(){
-    let value = this.form.value;
-    if(this.url){
-      value.image = this.url;
-    }
-    let car = new Car(value);
+   onSubmit(){
+     let value = this.form.value;
+     if(this.url){
+       value.image = this.url;
+     }
+     let car = new Car(value);
     // let customPath = `cooperative/${this.coop}/car`;
     // this.carProvider.customPath(customPath);
     this.carProvider.save(car, this.key);
