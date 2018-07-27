3ff5e6565b4b28922ffd8b0a1a74666d8b1880ff
Andry <andriramananarivo@gmail.com>
2018-07-27 10:57:39 +0500

    image-widget in worker add  edit

--- src/pages/worker-add/worker-add.ts
+++ src/pages/worker-add/worker-add.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ModalController } from 'ionic-angular';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { NotificationProvider } from '../../providers/notification/notification';
 import { WorkerProvider } from '../../providers/worker/worker';
@@ -12,6 +12,7 @@ import { Car } from '../../models/car.model';
 import { Cooperative } from '../../models/cooperative.model';
 import { CooperativeProvider } from '../../providers/cooperative/cooperative';
 import { WorkerDetailPage } from '../worker-detail/worker-detail';
+import { ImageWidgetPage } from '../image-widget/image-widget'
 
 /**
  * Generated class for the WorkerAddPage page.
@@ -34,7 +35,7 @@ export class WorkerAddPage {
   workertypes: any;
   cooperative: Cooperative = new Cooperative();
 
- 	constructor(private workertypeProvider: WorkerTypeProvider,private cooperativeProvider: CooperativeProvider, private cameraProvider: CameraProvider, private workerProvider: WorkerProvider, public notif: NotificationProvider, public platform:Platform, public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,public alertCtrl: AlertController) {
+ 	constructor(private modalCtrl : ModalController, private workertypeProvider: WorkerTypeProvider,private cooperativeProvider: CooperativeProvider, private cameraProvider: CameraProvider, private workerProvider: WorkerProvider, public notif: NotificationProvider, public platform:Platform, public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder,public alertCtrl: AlertController) {
  		this.form = this.formBuilder.group({
  			name: ['',Validators.required],			
  			matricule: ['',Validators.required],	
@@ -42,6 +43,7 @@ export class WorkerAddPage {
        tel: [, Validators.required]	
 
  		});
+     this.url = "assets/icon/camera";
  	}
 
  	ionViewDidLoad() {
@@ -82,22 +84,20 @@ export class WorkerAddPage {
     
   }
 
-  fromGallery(){
-    this.cameraProvider.selectPhoto().then((image)=>{
-      this.image = image;
-      this.workerProvider.uploadImage(this.image).then((url)=>{
-        this.url = url;
-      })
-    })
-  }
+  imageWidget(){
+     let modal = this.modalCtrl.create(ImageWidgetPage, null, {cssClass:'pricebreakup' });
 
-  fromCamera(){
-    this.cameraProvider.takePhoto().then((image)=>{
-      this.image = image;
-      this.workerProvider.uploadImage(this.image).then((url)=>{
+     modal.onDidDismiss(image => {
+       if (image) {
+         this.image = image;
+         this.workerProvider.uploadImage(this.image).then((url)=>{
         this.url = url;
       })
-    })
-  }
+       }
+     })
+
+     modal.present();
+
+   }
 
 }
