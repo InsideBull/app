9e4285303e1b522999e2591163e0930c36d6aa7a
Steve <steve@transporttek.org>
2018-08-03 10:52:46 +0300

    booking class et correction

--- src/pages/booking-class-liste/booking-class-liste.ts
+++ src/pages/booking-class-liste/booking-class-liste.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BookingClassProvider } from '../../providers/booking-class/booking-class';
 import { BookingClassDetailsPage } from '../booking-class-details/booking-class-details';
-
+import { BookingClass } from '../../models/booking-class.model';
 /**
  * Generated class for the BookingClassListePage page.
  *
@@ -20,6 +20,9 @@ export class BookingClassListePage {
   bookingClasses: any;
   cooperativeKey: any;
   empty = false;
+  bookingClass: BookingClass;
+  key: any; 
+
   constructor(private bookingClassProvider: BookingClassProvider, 
               public navCtrl: NavController, 
               public navParams: NavParams) {
@@ -29,6 +32,29 @@ export class BookingClassListePage {
   ionViewWillEnter() {}
 
   toConstruct(){
+    this.bookingClasses = [];
+  
+    this.cooperativeKey = this.navParams.get('cooperativeKey');
+  
+    let path = `cooperative/${this.cooperativeKey}/booking_class`;
+  
+    this.bookingClassProvider.customPath(path);
+  
+    this.bookingClassProvider.fetcAll().subscribe((bookingClasses)=>{
+      if (bookingClasses) {
+        for(let key in bookingClasses){
+        bookingClasses[key].key = key;
+        this.bookingClassProvider.fetch(bookingClasses[key].data).then((data)=>{
+          bookingClasses[key].data = data;
+        })
+  
+        this.bookingClasses.push(bookingClasses[key]);
+      }
+      }
+      else{
+        this.empty = true;
+      }
+    });
   }
 
   showDetails(key){
