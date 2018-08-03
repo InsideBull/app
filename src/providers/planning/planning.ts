9954c68e800ef39fe92b222aa5c91ecd29a833eb
Andry <andriramananarivo@gmail.com>
2018-08-03 10:38:30 +0500

    planning provider definition

--- src/providers/planning/planning.ts
+++ src/providers/planning/planning.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseProvider } from '../firebase/firebase';
+import { Planning } from '../../models/planning.model';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { IonicPage, AlertController, LoadingController, ToastController } from 'ionic-angular';
 
 /*
   Generated class for the PlanningProvider provider.
@@ -8,10 +12,32 @@ import { Injectable } from '@angular/core';
   and Angular DI.
 */
 @Injectable()
-export class PlanningProvider {
+export class PlanningProvider extends FirebaseProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello PlanningProvider Provider');
+  protected __path = 'planning';
+  
+  constructor(public storage: AngularFireStorage, public firebase: AngularFireDatabase, public loadingCtrl : LoadingController) {
+    super(storage,firebase,loadingCtrl);
+  }
+
+  save(station: Planning, key ?: string){
+  	if (key) {
+  		return this.push(station,key);
+  	}
+  	else{
+  		return this.push(station);
+  	}
+  }
+ 
+  fetch(key: string){
+  	return this.getOneById(key);
+  }
+
+  fetcAll(){
+  	return this.getAll();
+  }
+  deleteStation(i: string){
+    this.delete(i);
   }
 
 }
